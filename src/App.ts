f50c7eb63789fc54207bacfa4287d9f0bc4cb5bf
Chinsan Chung <chinsanchung@gmail.com>
2021-09-21 17:31:27 +0900

    App.ts -> app.ts 로 파일의 이름을 변경

--- src/App.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-
-const app: express.Application = express();
-
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("initial test");
-});
-
-export default app;
