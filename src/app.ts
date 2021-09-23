bd1d1b8bf3fc94d81aa4cdaf2b236ce4d59d4860
Chinsan Chung <chinsanchung@gmail.com>
2021-09-23 09:29:24 +0900

    auth 라우트를 app 에 연결. 임시로 세션과 passport 를 닫음

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import passport from 'passport';
 import PassportConfig from './middlewares/passportConfig';
 import Debugger from './utils/debugger';
 import Database from './middlewares/database';
+import routesList from './routes';
 
 dotenv.config();
 
@@ -35,31 +36,42 @@ export default class App extends Error {
         res: express.Response,
         next: express.NextFunction
       ) => {
-        if (process.env.NODE_ENV === 'production') morgan('combined');
-        else morgan('dev');
+        if (process.env.NODE_ENV === 'production') {
+          morgan('combined')(req, res, next);
+        } else {
+          morgan('dev')(req, res, next);
+        }
       }
     );
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use(cookieParser(process.env.COOKIE_SECRET));
-    this.app.use(
-      session({
-        resave: false,
-        saveUninitialized: false,
-        secret: process.env.COOKIE_SECRET,
-        cookie: { httpOnly: true, secure: false },
-        // store: new this.RedisStore({ client: this.redisClient }),
-      })
-    );
-    this.app.use(passport.initialize());
-    this.app.use(passport.session());
+    // this.app.use(
+    //   session({
+    //     resave: false,
+    //     saveUninitialized: false,
+    //     secret: process.env.COOKIE_SECRET,
+    //     cookie: { httpOnly: true, secure: false },
+    //     // store: new this.RedisStore({ client: this.redisClient }),
+    //   })
+    // );
+    // this.app.use(passport.initialize());
+    // this.app.use(passport.session());
+  };
+
+  private setRoutes = (): void => {
+    for (const route of routesList) {
+      this.app.use(route.path, route.router);
+    }
   };
 
   public initialize = (): void => {
     Database.init(process.env.MONGO_URI)
       .then(() => {
         this.setApplication();
-        this.app.listen(3000, () => Debugger.log('서버 시작'));
+        this.setRoutes();
+
+        this.app.listen(3000, () => Debugger.log('포트 3000. 서버 시작'));
       })
       .catch((error: Error) => {
         Debugger.error(error);
