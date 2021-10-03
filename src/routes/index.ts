179cc77f041ec316a27230df8660c3fa229d42eb
Chinsan Chung <chinsanchung@gmail.com>
2021-10-03 23:06:58 +0900

    user 라우터 연결

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import authRouter from './auth';
 import tweetRouter from './tweet';
 import readingRouter from './reading';
+import userRouter from './user';
 
 const routesList: { path: string; router: express.Router }[] = [
   { path: '/auth', router: authRouter },
   { path: '/tweet', router: tweetRouter },
   { path: '/reading', router: readingRouter },
+  { path: '/user', router: userRouter },
 ];
 
 export default routesList;
