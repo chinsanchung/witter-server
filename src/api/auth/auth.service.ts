fcdd01bfe01eb430fabdf090159d548cf91d0075
Chinsan Chung <chinsanchung@gmail.com>
2021-10-03 23:00:41 +0900

    가입 시 이메일과 아이디를 체크하는 함수를 작성, 가입 에러 처리 수정

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -5,18 +5,45 @@ import Debugger from '../../utils/debugger';
 import { JoinDto, IAuthService } from './auth.interface';
 
 export default class AuthService implements IAuthService {
+  private checkEmailDuplicate = async (email: string): Promise<boolean> => {
+    const response = await UserModel.findOne({ email }).lean();
+    if (response) {
+      Debugger.log('중복 이메일');
+      return true; // 중복
+    } else {
+      return false;
+    }
+  };
+  private checkIdDuplicate = async (user_id: string): Promise<boolean> => {
+    const response = await UserModel.findOne({ user_id }).lean();
+    if (response) {
+      Debugger.log('중복 이메일');
+      return true; // 중복
+    } else {
+      return false;
+    }
+  };
   // 아이디, 이메일 중복 확인 함수 넣기.
   join = async (user: JoinDto): Promise<IUser> => {
     try {
-      // Debugger.log('서비스에서 가입 시작');
-      const newUser = await UserModel.create(user);
-      // Debugger.log('새 사용자', newUser);
-      // 새로운 타임라인 추가
-      await TimeLineModel.create({ user_id: newUser.user_id });
-      return newUser;
+      const isDuplicateEmail = await this.checkEmailDuplicate(user.email);
+      const isDuplicateId = await this.checkIdDuplicate(user.user_id);
+      if (isDuplicateEmail && isDuplicateId) {
+        const message: string = `이미 존재하는 ${
+          isDuplicateId ? '이메일' : '아이디'
+        }입니다.`;
+        Debugger.error(message);
+        throw createError(403, message);
+      } else {
+        // Debugger.log('서비스에서 가입 시작');
+        const newUser = await UserModel.create(user);
+        // Debugger.log('새 사용자', newUser);
+        // 새로운 타임라인 추가
+        await TimeLineModel.create({ user_id: newUser.user_id });
+        return newUser;
+      }
     } catch (error) {
-      Debugger.error(error);
-      throw createError(error.status, error.message);
+      throw error;
     }
   };
 }
