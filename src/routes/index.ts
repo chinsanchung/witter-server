c0b4e76ac26996ee56599a883cb4a4fe11313cd5
Chinsan Chung <chinsanchung@gmail.com>
2021-10-04 00:07:36 +0900

    라우트를 배열이 아니라 express.Router 에 담아 '/api' 으로 사용하도록 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,18 @@ import tweetRouter from './tweet';
 import readingRouter from './reading';
 import userRouter from './user';
 
-const routesList: { path: string; router: express.Router }[] = [
-  { path: '/auth', router: authRouter },
-  { path: '/tweet', router: tweetRouter },
-  { path: '/reading', router: readingRouter },
-  { path: '/user', router: userRouter },
-];
+const router = express.Router();
+router.use('/auth', authRouter);
+router.use('/tweet', tweetRouter);
+router.use('/reading', readingRouter);
+router.use('/user', userRouter);
 
-export default routesList;
+export default router;
+// const routesList: { path: string; router: express.Router }[] = [
+//   { path: '/auth', router: authRouter },
+//   { path: '/tweet', router: tweetRouter },
+//   { path: '/reading', router: readingRouter },
+//   { path: '/user', router: userRouter },
+// ];
+
+// export default routesList;
