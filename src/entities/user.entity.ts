8ab40200488035541497e01b7920d037370028ff
Chinsan Chung <chinsanchung@gmail.com>
2021-12-30 15:49:54 +0900

    feat: class-validator를 통한 검증
    - class-validator를 이용해 컬럼을 검증하는 내용을 추가했습니다.
    - 향후 dto 를 작성할 때 활용합니다.
    
    resolves: #15

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -8,6 +8,7 @@ import {
 } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { InternalServerErrorException } from '@nestjs/common';
+import { IsOptional, IsString, Length, MaxLength } from 'class-validator';
 
 @Entity()
 export class User {
@@ -15,13 +16,20 @@ export class User {
   id: number;
 
   @Column()
+  @IsString()
+  @Length(5, 12, { message: 'user_id: 5 ~ 12 글자로 입력해주세요.' })
   user_id: string;
 
   @Column()
+  @IsString()
+  @Length(5, 20, { message: 'password: 5 ~ 20 글자를 입력해주세요.' })
   password: string;
 
   @Column({ default: '' })
-  description: string;
+  @IsString()
+  @MaxLength(50, { message: 'description: 50 글자 이내로 입력해주세요.' })
+  @IsOptional()
+  description?: string;
 
   @CreateDateColumn({ type: 'datetime' })
   created_at: Date;
