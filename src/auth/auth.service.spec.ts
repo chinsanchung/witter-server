a284cd8eafca907218de9441140b2d678ac7e6a4
Chinsan Chung <chinsanchung@gmail.com>
2022-01-02 17:23:05 +0900

    test: 로그인 인증 테스트 케이스 작성
    - 로그인 인증에 대한 테스트 케이스를 작성했습니다.
    - JWT 토큰을 검증하여 에러 또는 새로운 액세스 토큰을 발급하거나 둘 다 유효하면 유저 데이터를 리턴합니다.
    
    resolves: #13

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -130,4 +130,108 @@ describe('AuthService', () => {
       });
     });
   });
+
+  describe('verifyUserAuth', () => {
+    const defaultErrorOutput = { ok: false, httpStatus: 401 };
+
+    it('실패 - 토큰이 존재하지 않습니다.', async () => {
+      const errorAuthInput = {
+        accessToken: undefined,
+        refreshToken: undefined,
+      };
+      const errorOutput = {
+        ...defaultErrorOutput,
+        error: '로그인이 필요한 기능입니다.',
+      };
+      jest
+        .spyOn(service, 'checkTokenValidation')
+        .mockResolvedValue(errorOutput);
+
+      const result = await service.verifyUserAuth(errorAuthInput);
+
+      expect(service.checkTokenValidation).toHaveBeenCalledTimes(1);
+      expect(service.checkTokenValidation).toHaveBeenCalledWith(errorAuthInput);
+      expect(service.checkTokenValidation).toEqual(errorOutput);
+
+      expect(result).toEqual(errorOutput);
+    });
+
+    it('실패 - 유효한 토큰이 아닙니다.', async () => {
+      const authInput = {
+        accessToken: 'not-signed-token',
+        refreshToken: 'not-signed-token',
+      };
+      const errorOutput = {
+        ...defaultErrorOutput,
+        error: '유효한 토큰이 아닙니다.',
+      };
+      jest
+        .spyOn(service, 'checkTokenValidation')
+        .mockResolvedValue(errorOutput);
+
+      const result = await service.verifyUserAuth(authInput);
+
+      expect(service.checkTokenValidation).toHaveBeenCalledTimes(1);
+      expect(service.checkTokenValidation).toHaveBeenCalledWith(authInput);
+      expect(service.checkTokenValidation).toEqual(errorOutput);
+
+      expect(result).toEqual(errorOutput);
+    });
+
+    it('실패 - 엑세스 토큰 만료, 리프레시 토큰 유효, 새 액세스 토큰 발급', async () => {
+      const authInput = {
+        accessToken: undefined,
+        refreshToken: 'signed-token',
+      };
+      const errorOutput = {
+        ...defaultErrorOutput,
+        error: 'RETURN_NEW_TOKEN',
+      };
+      jest
+        .spyOn(service, 'checkTokenValidation')
+        .mockResolvedValue(errorOutput);
+
+      const result = await service.verifyUserAuth(authInput);
+
+      expect(service.checkTokenValidation).toHaveBeenCalledTimes(1);
+      expect(service.checkTokenValidation).toHaveBeenCalledWith(authInput);
+
+      expect(result).toEqual({
+        ...defaultErrorOutput,
+        data: { accessToken: 'signed-token' },
+      });
+    });
+
+    it('성공 - 모든 토큰 유효함, 유저 데이터를 리턴', async () => {
+      const authInput = {
+        accessToken: 'signed-token',
+        refreshToken: 'signed-token',
+      };
+      const outputWithAccessToken = {
+        ok: true,
+        data: { user_id: 'testid' },
+      };
+      const mockUser = {
+        id: 1,
+        user_id: 'testid',
+        password: '12345',
+        created_at: new Date(),
+        hashPassword: jest.fn(),
+      };
+      jest
+        .spyOn(service, 'checkTokenValidation')
+        .mockResolvedValue(outputWithAccessToken);
+      usersRepository.findOne.mockResolvedValue(mockUser);
+
+      const result = await service.verifyUserAuth(authInput);
+
+      expect(service.checkTokenValidation).toHaveBeenCalledTimes(1);
+      expect(service.checkTokenValidation).toHaveBeenCalledWith(authInput);
+
+      expect(result).toEqual({
+        ok: true,
+        data: mockUser,
+      });
+    });
+  });
 });
