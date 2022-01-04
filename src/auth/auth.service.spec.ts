983fc1481e92e5c44c599fd56bfc7a1ab7ded0cf
Chinsan Chung <chinsanchung@gmail.com>
2022-01-04 21:57:20 +0900

    fix: verifyUserAuth 테스트 케이스 삭제, verifyToken 테스트 케이스 작성
    - passport-jwt, @nestjs/passport 를 이용해 토큰을 검증하면서, verifyUserAuth 테스트 케이스를 제거했습니다.
    - verifyToken 메소드는 토큰을 검증하는 Guard 에서 토큰 문자열을 해석하여 유저 아이디를 리턴하는 역할을 맡고 있습니다.
    
    resolves: #13

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,12 +4,17 @@ import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { User } from 'src/entities/user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 const mockRepository = () => ({
   findOne: jest.fn(),
 });
 const mockJwtService = () => ({
   sign: jest.fn(() => 'signed-token'),
+  verifyAsync: jest.fn(),
+});
+const mockConfigService = () => ({
+  get: jest.fn(() => 'secret-key'),
 });
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
@@ -30,6 +35,10 @@ describe('AuthService', () => {
         {
           provide: JwtService,
           useValue: mockJwtService(),
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService(),
         },
       ],
     }).compile();
@@ -131,106 +140,37 @@ describe('AuthService', () => {
     });
   });
 
-  describe('verifyUserAuth', () => {
-    const defaultErrorOutput = { ok: false, httpStatus: 401 };
+  describe('verifyToken', () => {
+    it('실패 - 토큰이 유효하지 않습니다.', async () => {
+      const token = 'invalid-token';
+      jest.spyOn(jwtService, 'verifyAsync').mockRejectedValue(new Error());
 
-    it('실패 - 토큰이 존재하지 않습니다.', async () => {
-      const errorAuthInput = {
-        accessToken: undefined,
-        refreshToken: undefined,
-      };
-      const errorOutput = {
-        ...defaultErrorOutput,
-        error: '로그인이 필요한 기능입니다.',
-      };
-      jest
-        .spyOn(service, 'checkTokenValidation')
-        .mockResolvedValue(errorOutput);
+      const result = await service.verifyToken(token);
 
-      const result = await service.verifyUserAuth(errorAuthInput);
-
-      expect(service.checkTokenValidation).toHaveBeenCalledTimes(1);
-      expect(service.checkTokenValidation).toHaveBeenCalledWith(errorAuthInput);
-      expect(service.checkTokenValidation).toEqual(errorOutput);
-
-      expect(result).toEqual(errorOutput);
-    });
-
-    it('실패 - 유효한 토큰이 아닙니다.', async () => {
-      const authInput = {
-        accessToken: 'not-signed-token',
-        refreshToken: 'not-signed-token',
-      };
-      const errorOutput = {
-        ...defaultErrorOutput,
-        error: '유효한 토큰이 아닙니다.',
-      };
-      jest
-        .spyOn(service, 'checkTokenValidation')
-        .mockResolvedValue(errorOutput);
-
-      const result = await service.verifyUserAuth(authInput);
-
-      expect(service.checkTokenValidation).toHaveBeenCalledTimes(1);
-      expect(service.checkTokenValidation).toHaveBeenCalledWith(authInput);
-      expect(service.checkTokenValidation).toEqual(errorOutput);
-
-      expect(result).toEqual(errorOutput);
-    });
-
-    it('실패 - 엑세스 토큰 만료, 리프레시 토큰 유효, 새 액세스 토큰 발급', async () => {
-      const authInput = {
-        accessToken: undefined,
-        refreshToken: 'signed-token',
-      };
-      const errorOutput = {
-        ...defaultErrorOutput,
-        error: 'RETURN_NEW_TOKEN',
-      };
-      jest
-        .spyOn(service, 'checkTokenValidation')
-        .mockResolvedValue(errorOutput);
-
-      const result = await service.verifyUserAuth(authInput);
-
-      expect(service.checkTokenValidation).toHaveBeenCalledTimes(1);
-      expect(service.checkTokenValidation).toHaveBeenCalledWith(authInput);
-
+      expect(jwtService.verifyAsync).toHaveBeenCalledTimes(1);
+      expect(jwtService.verifyAsync).toHaveBeenCalledWith(token, {
+        secret: 'secret-key',
+      });
       expect(result).toEqual({
-        ...defaultErrorOutput,
-        data: { accessToken: 'signed-token' },
+        ok: false,
+        error: 'INVALID_TOKEN',
       });
     });
 
-    it('성공 - 모든 토큰 유효함, 유저 데이터를 리턴', async () => {
-      const authInput = {
-        accessToken: 'signed-token',
-        refreshToken: 'signed-token',
-      };
-      const outputWithAccessToken = {
-        ok: true,
-        data: { user_id: 'testid' },
-      };
-      const mockUser = {
-        id: 1,
-        user_id: 'testid',
-        password: '12345',
-        created_at: new Date(),
-        hashPassword: jest.fn(),
-      };
-      jest
-        .spyOn(service, 'checkTokenValidation')
-        .mockResolvedValue(outputWithAccessToken);
-      usersRepository.findOne.mockResolvedValue(mockUser);
+    it('성공 - 유저 아이디를 리턴합니다.', async () => {
+      const token = 'signed-token';
+      const tokenPayload = { user_id: 'testid' };
+      jest.spyOn(jwtService, 'verifyAsync').mockResolvedValue(tokenPayload);
 
-      const result = await service.verifyUserAuth(authInput);
+      const result = await service.verifyToken(token);
 
-      expect(service.checkTokenValidation).toHaveBeenCalledTimes(1);
-      expect(service.checkTokenValidation).toHaveBeenCalledWith(authInput);
-
+      expect(jwtService.verifyAsync).toHaveBeenCalledTimes(1);
+      expect(jwtService.verifyAsync).toHaveBeenCalledWith(token, {
+        secret: 'secret-key',
+      });
       expect(result).toEqual({
         ok: true,
-        data: mockUser,
+        data: tokenPayload,
       });
     });
   });
