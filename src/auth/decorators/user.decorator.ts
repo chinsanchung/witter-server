b3a368e5f11d4ba1344df0e0f9224206b2f2cb84
Chinsan Chung <chinsanchung@gmail.com>
2022-01-04 23:48:42 +0900

    fix: 리턴 타입 명시
    - UserId 데코레이터에 리턴 타입으로 유저 엔티티를 추가했습니다.
    
    resolves: #3

--- src/auth/decorators/user.decorator.ts
+++ src/auth/decorators/user.decorator.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
 
 export const UserId = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (data: unknown, ctx: ExecutionContext): User => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
   },
